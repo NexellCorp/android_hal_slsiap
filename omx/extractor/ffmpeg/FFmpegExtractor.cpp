29f794405f583bcdb92fe534dcdfe6992bf9c353
SeongO Park <ray@nexell.co.kr>
2015-02-04 14:21:06 +0900

    * Add unsupported audio codec filtering routine.

--- omx/extractor/ffmpeg/FFmpegExtractor.cpp
+++ omx/extractor/ffmpeg/FFmpegExtractor.cpp
@@ -1066,6 +1066,82 @@ void FFmpegExtractor::setFFmpegDefaultOpts()
 	mEOF          = false;
 }
 
+
+int av_find_best_audio_stream(AVFormatContext *ic,
+	                          enum AVMediaType type,
+	                          int wanted_stream_nb,
+	                          int related_stream,
+	                          AVCodec **decoder_ret,
+	                          int flags)
+{
+	int i, nb_streams = ic->nb_streams;
+	int ret = AVERROR_STREAM_NOT_FOUND, best_count = -1, best_bitrate = -1, best_multiframe = -1, count, bitrate, multiframe;
+	unsigned *program = NULL;
+
+	if (related_stream >= 0 && wanted_stream_nb < 0)
+	{
+		AVProgram *p = av_find_program_from_stream(ic, NULL, related_stream);
+		if (p) {
+			program = p->stream_index;
+			nb_streams = p->nb_stream_indexes;
+		}
+	}
+
+	for (i = 0; i < nb_streams; i++)
+	{
+		int real_stream_index = program ? program[i] : i;
+		AVStream *st = ic->streams[real_stream_index];
+		AVCodecContext *avctx = st->codec;
+		if (avctx->codec_type != type)
+			continue;
+		if (wanted_stream_nb >= 0 && real_stream_index != wanted_stream_nb)
+			continue;
+		if (st->disposition & (AV_DISPOSITION_HEARING_IMPAIRED|AV_DISPOSITION_VISUAL_IMPAIRED))
+			continue;
+
+		switch(avctx->codec_id)
+		{
+			case CODEC_ID_AAC:
+			case CODEC_ID_AC3:
+			case CODEC_ID_MP1:
+			case CODEC_ID_MP2:
+			case CODEC_ID_MP3:
+			case CODEC_ID_WMAV1:
+			case CODEC_ID_WMAV2:
+			case CODEC_ID_WMAPRO:
+			case CODEC_ID_WMALOSSLESS:
+			case CODEC_ID_COOK:
+			case CODEC_ID_APE:
+			case CODEC_ID_DTS:
+			case CODEC_ID_FLAC:
+			case CODEC_ID_PCM_S16LE:
+				break;
+			default:
+				continue;
+		}
+
+		count = st->codec_info_nb_frames;
+		bitrate = avctx->bit_rate;
+		multiframe = FFMIN(5, count);
+		if ((best_multiframe >  multiframe) ||
+			(best_multiframe == multiframe && best_bitrate >  bitrate) ||
+			(best_multiframe == multiframe && best_bitrate == bitrate && best_count >= count))
+			continue;
+
+		best_count = count;
+		best_bitrate = bitrate;
+		best_multiframe = multiframe;
+		ret = real_stream_index;
+		if (program && i == nb_streams - 1 && ret < 0) {
+			program = NULL;
+			nb_streams = ic->nb_streams;
+			i = 0; /* no related stream found, try again with everything */
+		}
+	}
+	return ret;
+}
+
+
 int FFmpegExtractor::initStreams()
 {
 	int err, i;
@@ -1131,8 +1207,10 @@ int FFmpegExtractor::initStreams()
 	}
 	if (!mAudioDisable)
 	{
-		st_index[AVMEDIA_TYPE_AUDIO] = av_find_best_stream(mFormatCtx, AVMEDIA_TYPE_AUDIO,
-														   wanted_stream[AVMEDIA_TYPE_AUDIO], st_index[AVMEDIA_TYPE_VIDEO], NULL, 0);
+		// st_index[AVMEDIA_TYPE_AUDIO] = av_find_best_stream(mFormatCtx, AVMEDIA_TYPE_AUDIO,
+		// 												   wanted_stream[AVMEDIA_TYPE_AUDIO], st_index[AVMEDIA_TYPE_VIDEO], NULL, 0);
+		st_index[AVMEDIA_TYPE_AUDIO] = av_find_best_audio_stream(mFormatCtx, AVMEDIA_TYPE_AUDIO,
+																wanted_stream[AVMEDIA_TYPE_AUDIO], st_index[AVMEDIA_TYPE_VIDEO], NULL, 0);
 	}
 	if (mShowStatus) {
 		av_dump_format(mFormatCtx, 0, mFilename, 0);
