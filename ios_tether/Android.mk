e6f02abcd0f78c8a88eca27ab89f68ee011647ce
Jongsin Park <pjsin865@nexell.co.kr>
2016-01-11 11:49:22 +0900

    -- iOS iAP/Tethering : change default mode and check ECID.

--- ios_tether/Android.mk
+++ ios_tether/Android.mk
@@ -6,6 +6,7 @@ include $(LOCAL_PATH)/android/jni/libiconv.mk \
 	$(LOCAL_PATH)/android/jni/libusbmuxd.mk \
 	$(LOCAL_PATH)/android/jni/libusb.mk \
 	$(LOCAL_PATH)/android/jni/libimobiledevice.mk \
+	$(LOCAL_PATH)/android/jni/libxml2.mk \
 	$(LOCAL_PATH)/android/jni/usbmuxd.mk
 
 endif
